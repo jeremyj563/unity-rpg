db1aac35c0eb3dc3d80c0d90a73c8f76a8fa3281
Jeremy Johnson <dummy.bin@hotmail.com>
2020-07-11 00:11:52 -0500

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rigidbody;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var xInput = Input.GetAxisRaw("Horizontal");
+        var yInput = Input.GetAxisRaw("Vertical");
+        var velocity = new Vector2(xInput, yInput) * moveSpeed;
+        rigidbody.velocity = velocity;
+    }
+}
