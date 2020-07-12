4ae2545d9ae58803df2aa39a538b81a1fd4ec876
Jeremy Johnson <dummy.bin@hotmail.com>
2020-07-11 21:02:48 -0500

    Lecture 19

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour {
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (PlayerController.Instance == null) {
+            Instantiate(this.Player);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
