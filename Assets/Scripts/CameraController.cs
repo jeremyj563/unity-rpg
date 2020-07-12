225b898371eb5dd512a94b40ac2d98793080d3d4
Jeremy Johnson <dummy.bin@hotmail.com>
2020-07-11 21:30:02 -0500

    Lecture 20

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.Target = PlayerController.Instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate() {
+        SetPosition(this.Target.position);
+    }
+
+    private void SetPosition(Vector3 target) {
+        var currentPosition = base.transform.position;
+        var x = target.x;
+        var y = target.y;
+        var z = currentPosition.z;
+        base.transform.position = new Vector3(x, y, z);
+    }
+}
