8ad76d144690ed6c5b2c0c4ad91be767abb9df82
Jeremy Johnson <dummy.bin@hotmail.com>
2020-07-11 20:36:11 -0500

    Lecture 18

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public static PlayerController Instance { get; set; }
+    public Rigidbody2D Rigidbody;
+    public float MoveSpeed;
+    public Animator Animator;
+    public string Transition;
 
-    public Rigidbody2D rigidbody;
-    public float moveSpeed;
+    private Vector2 _Velocity {
+        get {
+            return this.Rigidbody.velocity;
+        } set {
+            this.Rigidbody.velocity = value;
+        }
+    }
+    private float _XInput { get; set; }
+    private float _YInput { get; set; }
 
     // Start is called before the first frame update
-    void Start() {
-
+    private void Start() {
+        SetInstance();
+        DontDestroyOnLoad(base.gameObject);
     }
 
     // Update is called once per frame
-    void Update() {
-        var xInput = Input.GetAxisRaw("Horizontal");
-        var yInput = Input.GetAxisRaw("Vertical");
-        var velocity = new Vector2(xInput, yInput) * moveSpeed;
-        rigidbody.velocity = velocity;
+    private void Update() {
+        GetXYInput();
+        SetVelocity();
+        Animate();
+    }
+
+    private void SetInstance() {
+        if (PlayerController.Instance == null) {
+            PlayerController.Instance = this;
+        } else {
+            Destroy(base.gameObject);
+        }
+    }
+
+    private void GetXYInput() {
+        this._XInput = Input.GetAxisRaw("Horizontal");
+        this._YInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void SetVelocity() {
+        this._Velocity = new Vector2(this._XInput, this._YInput) * MoveSpeed;
+    }
+
+    private void SetLastMove(float x, float y) {
+        this.Animator.SetFloat("lastMoveX", x);
+        this.Animator.SetFloat("lastMoveY", y);
+    }
+
+    private void Animate() {
+        this.Animator.SetFloat("moveX", this._Velocity.x);
+        this.Animator.SetFloat("moveY", this._Velocity.y);
+        
+        float[] xy = {this._XInput, this._YInput};
+        if (xy.Contains(1) || xy.Contains(-1)) {
+            SetLastMove(this._XInput, this._YInput);
+        }
+    }
+
+    private string GetDebuggerDisplay() {
+        return ToString();
     }
 }
---
+++ Assets/Scripts/SceneEntrance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneEntrance : MonoBehaviour {
+    public string Transition;
+
+    // Start is called before the first frame update
+    void Start() {
+        SetPlayerPosition();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void SetPlayerPosition() {
+        var player = PlayerController.Instance;
+        if (this.Transition == player.Transition) {
+            player.transform.position = base.transform.position;
+        }
+    }
+}
