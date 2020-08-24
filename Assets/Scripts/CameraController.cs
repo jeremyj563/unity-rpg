4ab791b718dcd3fa122143a2f44b186877d3fe9d
Jeremy Johnson <dummy.bin@hotmail.com>
2020-08-24 14:47:58 -0500

    upgraded unity to v2020.1.3f1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour {
     public Transform Target;
+    public Tilemap Map;
+
+    private float HalfHeight;
+    private float HalfWidth;
+    private Vector3 MinBoundary;
+    private Vector3 MaxBoundary;
 
     // Start is called before the first frame update
     void Start() {
         this.Target = PlayerController.Instance.transform;
+        SetBoundaries();
     }
 
     // Update is called once per frame
@@ -17,14 +25,27 @@ public class CameraController : MonoBehaviour {
 
     // LateUpdate is called once per frame after Update
     void LateUpdate() {
-        SetPosition(this.Target.position);
+        //var position = base.transform.position;
+        //var x = Mathf.Clamp(position.x, this.MinBoundary.x, this.MaxBoundary.x);
+        //var y = Mathf.Clamp(position.y, this.MinBoundary.y, this.MaxBoundary.y);
+        //var z = position.z;
+        //base.transform.position = new Vector3(x, y, z);
+        //SetPosition(this.Target.position);
+
+        base.transform.position = new Vector3(this.Target.position.x, this.Target.position.y, base.transform.position.z);
+        base.transform.position = new Vector3(Mathf.Clamp(base.transform.position.x, this.MinBoundary.x, this.MaxBoundary.x), Mathf.Clamp(base.transform.position.y, this.MinBoundary.y, this.MaxBoundary.y), base.transform.position.z);
+    }
+
+    private void SetBoundaries() {
+        this.HalfHeight = Camera.main.orthographicSize;
+        this.HalfWidth = Camera.main.aspect * this.HalfHeight;
+        this.MinBoundary = this.Map.localBounds.min;
+        this.MinBoundary += new Vector3(this.HalfWidth, this.HalfHeight, 0f);
+        this.MaxBoundary = this.Map.localBounds.max;
+        this.MaxBoundary -= new Vector3(this.HalfWidth, this.HalfHeight, 0f);
     }
 
     private void SetPosition(Vector3 target) {
-        var currentPosition = base.transform.position;
-        var x = target.x;
-        var y = target.y;
-        var z = currentPosition.z;
-        base.transform.position = new Vector3(x, y, z);
+
     }
 }
